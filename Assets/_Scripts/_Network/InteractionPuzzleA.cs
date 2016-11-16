334b9c7c0b76a6470b2f5e435bf51fee03a8f9c0
mauriciojuba <mauriciojuba@outlook.com>
2016-11-16 02:05:20 -0200

    Divisão de Telas Multiplayer / Assembly Puzzle 1(WIP)

---
+++ Assets/_Scripts/_Network/InteractionPuzzleA.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using GooglePlayGames;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InteractionPuzzleA : MonoBehaviour {
+
+    public GameObject _aController, _aCanvas, _bController, _bCanvas;
+
+
+	void Start () {
+        splitScreens();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void splitScreens()
+    {
+        if (InteractionPlayers.isAntonio)
+        {
+            _aController.SetActive(true);
+            _aCanvas.SetActive(true);
+            _bCanvas.SetActive(false);
+            _bController.SetActive(false);
+        }
+        else
+        {
+            _aController.SetActive(false);
+            _aCanvas.SetActive(false);
+            _bCanvas.SetActive(true);
+            _bController.SetActive(true);
+        }
+    }
+}
