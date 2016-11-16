37eb86633ade6aee6fd36cee694feb120eba097c
Wauei <waueigt@gmail.com>
2016-11-15 22:34:06 -0200

    Livro Puzzle 1

---
+++ Assets/_Scripts/_Capitulo_1/Jogador B/Livro.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TouchScript.Gestures;
+
+public class Livro : MonoBehaviour {
+
+	public Sprite[] Paginas;
+	public int PaginaAtual;
+	private Sprite ImagemPagina;
+
+
+	// Use this for initialization
+
+
+	void Start () {
+
+
+		PaginaAtual = 0;
+		ImagemPagina = Paginas [PaginaAtual];
+		gameObject.GetComponent<SpriteRenderer>().sprite = ImagemPagina;
+	
+	}
+		
+		
+	public void ProcimaPagina(){
+		
+		if (PaginaAtual >= 0 && PaginaAtual < Paginas.Length - 1) {
+			PaginaAtual++;
+			ImagemPagina = Paginas [PaginaAtual];
+			gameObject.GetComponent<SpriteRenderer> ().sprite = ImagemPagina;
+		}
+	}
+
+	public void PaginaAnterior(){
+
+		if (PaginaAtual > 0 && PaginaAtual <= Paginas.Length) {
+			PaginaAtual--;
+			ImagemPagina = Paginas [PaginaAtual];
+			gameObject.GetComponent<SpriteRenderer> ().sprite = ImagemPagina;
+
+		}
+	}
+}
---
+++ Assets/_Scripts/_Capitulo_1/Jogador B/MudaPag.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+
+public class MudaPag : MonoBehaviour {
+
+	private Vector3 PosInicial;
+	private TransformGesture Tc;
+
+
+	// Use this for initialization
+	void Start () {
+
+		PosInicial = gameObject.transform.position;
+		Tc = gameObject.GetComponent<TransformGesture>();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D hit){
+		Livro Virapag = GameObject.Find ("Livro").GetComponent<Livro> ();
+
+		if (hit.gameObject.name == "next") {
+			Tc.Cancel ();
+			Virapag.ProcimaPagina ();
+			gameObject.transform.position = PosInicial;
+		}
+
+		if (hit.gameObject.name == "prev") {
+			Tc.Cancel ();
+			Virapag.PaginaAnterior ();
+			gameObject.transform.position = PosInicial;
+		}
+	}
+}
