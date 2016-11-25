5a51039999851c15a04064b30d432cf2afd2dac0
mauriciojuba <mauriciojuba@outlook.com>
2016-11-25 04:10:27 -0200

    cutscene inicial completa

---
+++ Assets/cutscene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class cutscene : MonoBehaviour {
+
+    public void nextLevel()
+    {
+        SceneManager.LoadScene("Fase1A");
+    }
+	void Update () {
+	
+	}
+}
