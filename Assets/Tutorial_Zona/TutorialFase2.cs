c2bc1206a297cfde59e326ab9a8f289044a5b392
Wauei <waueigt@gmail.com>
2016-12-06 00:05:06 -0200

    Falas Tutorial Fase 2

---
+++ Assets/Tutorial_Zona/TutorialFase2.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript;
+
+public class TutorialFase2: MonoBehaviour {
+
+    public GameObject[] FalasJogadorA;
+    public GameObject[] FalasJogadorB;
+
+    public GameObject MaozinhaA, MaozinhaB;
+
+    public GameObject BloqueiaA, BloqueiaB;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+
+    //------------- Falas Jogador A -------------
+
+    public void AtivaFalaA(int fala)
+    {
+
+
+
+        FalasJogadorA[fala].SetActive(true);
+
+    }
+
+    public void DesativaFalaA(){
+
+
+
+        for(int i = 0; i <FalasJogadorA.Length; i++){
+            FalasJogadorA[i].SetActive(false);
+        }
+    }
+
+    //------------- Falas Jogador B -------------
+
+    public void AtivaFalaB(int fala){
+
+        BloqueiaB.SetActive(true);
+
+
+        FalasJogadorB[fala].SetActive(true);
+
+
+    }
+
+    public void DesativaFalaB(){
+
+        BloqueiaB.SetActive(false);
+
+        for (int i = 0; i < FalasJogadorB.Length; i++){
+            FalasJogadorB[i].SetActive(false);
+        }
+    }
+}
